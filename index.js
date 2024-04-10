cf880b817d4f0097e32ffdc980d0a72599caec25
Manuele Vancin <mfvancin@gmail.com>
2024-04-10 15:17:02 +0100

    LabDay3

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + ' ' + s2 + ' ' + s3 + ' ' + s4 + ' ' + s5 + ' ' + s3 + ' ' + s2 + ' ' +s1 + ' ' +s4;
 
 // Print out the concatenated string
-
+console.log (tongueTwister);
 
 
 
@@ -22,12 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
-// Print the cameLtaiL-formatted string
-
-
-
+const result = part1.charAt().toUpperCase() + part2.charAt().toUpperCase();
+//charAt(str.length-1)
+// Print the cameLtaiL-formatted string 
+console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,23 +33,20 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount = 84*(15/100);
 
 // Print out the tipAmount
-
-
-
+console.log(tipAmount);
 
 /*******************************************
-    Iteration 2.2 | Generate Random Number
+    Iteration 2.2 | Generate Random Number 
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.trunc(Math.random()*10);
 
 // Print the generated random number
-
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +56,21 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
+/*const a = true;
+const b = false;*/
 const expression1 = a && b;
-
+// False
 const expression2 = a || b;
-
+// True
 const expression3 = !a && b;
-
+// False
 const expression4 = !(a && b);
-
+// True
 const expression5 = !a || !b;
-
+// True
 const expression6 = !(a || b);
-
+// False
 const expression7 = a && a;
+// True
+
+console.log(expression1 + ' ' + expression2 + ' ' + expression3 + ' ' + expression4 + ' ' + expression5 + ' ' + expression6 + ' ' + expression7);
